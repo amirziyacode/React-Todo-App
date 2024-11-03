d197d90b6923e9fde600a6f3a661ce7e4de3b5f6
amirziyacode <amzdidi@gmail.com>
2024-11-03 12:52:47 +0330

    improve-design

--- src/componet/TodoRow.jsx
+++ src/componet/TodoRow.jsx
@@ -3,7 +3,7 @@ function TodoRow(props = {rowId:Number,rowDescription:String,rowTitle:String}) {
     return (
         <tr>
             <th scope="row">{props.rowId}</th>
-            <td>{props.rowTitle}</td>
+            <td >{props.rowTitle}</td>
             <td>{props.rowDescription}</td>
         </tr>
     )
--- src/componet/TodoTable.jsx
+++ src/componet/TodoTable.jsx
@@ -3,22 +3,24 @@ import TodoRow from "./TodoRow";
 
 function TodoTable(props = {todos:[]}){
     return(
-        <table className="table table-hover">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-            </tr>
-            </thead>
-            <tbody>
-            {props.todos.map(
-                todo => (
-                    <TodoRow key={todo.id} rowId={todo.id} rowTitle={todo.title} rowDescription={todo.description}/>
-                )
-            )}
-            </tbody>
-        </table>
+        <div className="mt-3">
+            <table className="table table-hover">
+                <thead className="table-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                </tr>
+                </thead>
+                <tbody>
+                {props.todos.map(
+                    todo => (
+                        <TodoRow key={todo.id} rowId={todo.id} rowTitle={todo.title} rowDescription={todo.description}/>
+                    )
+                )}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
