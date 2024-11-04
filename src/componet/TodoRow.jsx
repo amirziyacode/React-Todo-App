757e8132fee390f5e85e5abf1cbcfe175013d241
amirziyacode <amzdidi@gmail.com>
2024-11-04 22:35:40 +0330

    improve-design-and-delete-todos

--- src/componet/TodoRow.jsx
+++ src/componet/TodoRow.jsx
@@ -1,7 +1,12 @@
+import axios from "axios";
 
 function TodoRow(props = {rowId:Number,rowDescription:String,rowTitle:String}) {
+    const deleteFun = async () => {
+        await axios.delete(`http://localhost:8080/api/v1/todos/${props.rowId}`)
+        window.location.reload();
+    }
     return (
-        <tr>
+        <tr onClick={deleteFun}>
             <th scope="row">{props.rowId}</th>
             <td >{props.rowTitle}</td>
             <td>{props.rowDescription}</td>
