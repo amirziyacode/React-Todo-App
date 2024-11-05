d10be73338d8fd2bfcbd672abcdfc4c57a58b3a0
amirziyacode <amzdidi@gmail.com>
2024-11-05 09:41:04 +0330

    refactoring-Appjs-add-fake-data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import axios from "axios";
 import FormTodo from "./componet/FormTodo";
 
 function App() {
-    const [todoModel,setTodo] = useState([]);
+    const [todoModel,setTodo] = useState([
+        {
+            "id":1,
+            "title":"Sport",
+            "description":"Play Game !!",
+            "do":false
+        }
+
+    ]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(async () => {
+    useEffect( () => {
+        const fetchData =async () => {
             const response = await axios.get("http://localhost:8080/api/v1/todos")
             setTodo(response.data);
+        }
+        // fetchData();
     }, []);
   return (
       <div className="mt-5 container">
